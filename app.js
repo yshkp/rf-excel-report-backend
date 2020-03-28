10bf13d2ca1cb20b5eeea7d9e58aa4c734e21897
yash <yash.kapoor.min17@itbhu.ac.in>
2020-03-28 11:49:06 +0530

    Set Headers

--- app.js
+++ app.js
@@ -5,6 +5,17 @@ const app = express();
 const formRouter = require("./route");
 app.use(express.json());
 
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+    );
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE");
+  
+    next();
+  });
+
 app.use("/", formRouter);
 
 app.listen(7000, ()=> {console.log('server up and running!!')});
